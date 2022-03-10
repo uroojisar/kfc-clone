8c9256edee489d7d1283d7124cce605f94d43ef7
uroojisar <uroojisaar@gmail.com>
2022-03-10 17:36:11 +0500

    Create home page and navbar

---
+++ src/Pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homepage.styles.scss';
+import NavBar from '../../Components/navbar/navbar.component';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <NavBar/>
+        </div>
+    );
+}
+
+export default HomePage;
